9e81f2bff94ade8fc9d08b897aa8202da0fd34dd
ivaylo-mobile-dev <ivayloslav@hotmail.com>
2018-06-07 18:53:49 -0600

    replaced ion item dividers with ion list headers in Education and Exeperience sections

--- src/components/app-footer/app-footer.ts
+++ src/components/app-footer/app-footer.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 /**
  * Generated class for the AppFooterComponent component.
  *
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AppHeaderComponent } from './app-header/app-header';
 import { AppFooterComponent } from './app-footer/app-footer';
+
 @NgModule({
-	declarations: [AppHeaderComponent,
-    AppFooterComponent],
+	declarations: [
+	  AppHeaderComponent,
+    AppFooterComponent
+  ],
 	imports: [],
-	exports: [AppHeaderComponent,
-    AppFooterComponent]
+	exports: [
+	  AppHeaderComponent,
+    AppFooterComponent
+  ]
 })
 export class ComponentsModule {}
