43478636a4aa40a3f3b9322be5a7f3e20c704a19
ivaylo-mobile-dev <ivayloslav@hotmail.com>
2018-06-08 03:43:47 -0600

    added left and right padding on all pages, converted list deviders to list headers for year separation. removed bottom border from list headers in iOS. added hyperlinks for LinkedIn and GitHub in Contacts.

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -15,11 +15,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactPage {
 
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
   }
+
+  openUrl(){
+    window.open('https://www.youtube.com/embed/kEU9q1LOcI0', '_system');
+  }
+
+  openLinkedInUrl(){
+    window.open('https://www.linkedin.com/in/slavovivaylo/', '_system');
+  }
+
+  openGitHubUrl(){
+    window.open('https://github.com/ivaylo-mobile-dev', '_system');
+  }
+
 
 }
